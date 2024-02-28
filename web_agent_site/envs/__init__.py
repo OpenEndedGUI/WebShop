104fad1e7683433e80369566df046ad8265ee7b7
mert-cemri <79153625+mert-cemri@users.noreply.github.com>
2024-02-28 01:39:08 -0800

    Update __init__.py

--- web_agent_site/envs/__init__.py
+++ web_agent_site/envs/__init__.py
@@ -2,6 +2,7 @@ from gym.envs.registration import register
 
 from web_agent_site.envs.web_agent_site_env import WebAgentSiteEnv
 from web_agent_site.envs.web_agent_text_env import WebAgentTextEnv
+from web_agent_site.envs.web_agent_image_env import WebAgentImageEnv
 
 register(
   id='WebAgentSiteEnv-v0',
@@ -11,4 +12,9 @@ register(
 register(
   id='WebAgentTextEnv-v0',
   entry_point='web_agent_site.envs:WebAgentTextEnv',
+)
+
+register(
+  id='WebAgentImageEnv-v0',
+  entry_point='web_agent_site.envs:WebAgentImageEnv',
 )
